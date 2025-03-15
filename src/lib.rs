692595b6aef48380e12824c68f057f488ec819d4
Oliver Webb <oliverwebb8192@gmail.com>
2025-03-15 15:39:18 -0500

    move from codeberg to github

--- src/lib.rs
+++ src/lib.rs
@@ -8,23 +8,30 @@ of celestial objects, such as the moon, sun, planets, and stars.
 
 ```
 use pracstro::*;
+
 let now_date = time::Date::from_calendar(2025, 4, 16.0);
 let now_time = time::Period::from_clock(19, 41, 11.0);
 let my_latitude = time::Period::from_degrees(30.5);
 let my_longitude = time::Period::from_degrees(-110.0);
+
 sol::VENUS.location(now_date).horizon(now_date, now_time, my_latitude, my_longitude); // Get the horizontal coordinates of Venus
 moon::MOON.phase(now_date).0; // The illuminated fraction of the moons surface
 time::Period::from_degrees(120.0).clock(); // 16h00m00s
 ```
 
-# Speed
+# Benchmarks
 
-| Test           | Mean (n=40000) |
-|----------------|----------------|
-| Control (NOP)  | 0ns            |
-| Current time   | 34ns           |
-| Full ephemeris | 3.406µs        |
-| Moon Phase     | 558ns          |
+Although speed is not a direct goal of this library, the simplicity of the algorithms used often makes
+the library much faster than other astronomy libraries. The tradeoff for this simplicity is
+some accuracy, though the library remains accurate enough for most real-world use.
+
+Average of many tests, n = 40,000:
+
+| Test           | `pracstro`           | [`astro`](https://crates.io/crates/astro) |
+|----------------|----------------------|---------------------|
+| Moon Phase     | 558ns                | 2,979ns (3µs)       |
+| Jupiter Coords | 601ns                | 70,860ns (70µs)     |
+| Full ephemeris | 3,406ns (3.4µs)      | 1,208,833ns (1.2ms) |
 
 # Structure
 This library contains 4 primary modules, which build upon the ones before them:
